d91a3ee5b874fc597cb8a78e278e732c62e60371
shekharbora <chandu42011@gmail.com>
2016-09-12 12:59:20 +0530

    changes in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+
+def hi
+puts "changes buy chandra"
+
+end 
  
 end
